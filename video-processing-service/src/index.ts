744fe4775c6e9b7e34acbfd2c5d88bf20be14e8e
Spence <120441242+5pencer5mith@users.noreply.github.com>
2025-04-30 10:57:28 -0600

    Added cloud hosted video processing

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,35 +1,68 @@
 import express from 'express';
 import ffmpeg from 'fluent-ffmpeg';
 
+import {
+  uploadProcessedVideo,
+  downloadRawVideo,
+  deleteRawVideo,
+  deleteProcessedVideo,
+  convertVideo,
+  setupDirectories
+} from './storage';
+
+// Create directories for videos (local)
+setupDirectories();
+
 const app = express();
 app.use(express.json());
 
-app.post('/process-video', (req, res) => {
+app.post('/process-video', async (req, res) => {
 
-  // Get the path of the input video file from the request body
-  const inputFilePath = req.body.inputFilePath;
-  const outputFilePath = req.body.outputFilePath;
-
-  // Check if the input file path is defined
-  if (!inputFilePath || !outputFilePath) {
-    res.status(400).send('bad request: missing file path');
+  // get bucket and filename from cloud pub/sub message
+  let data;
+  try {
+    const message = Buffer.from(req.body.message.data, 'base64').toString('utf8');
+    data = JSON.parse(message);
+    if (!data.name) {
+      throw new Error('invalid message payload received.');
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(400).send('bad request: missing filename.')
+    return
   }
 
-  // Create the ffmpeg command
-  ffmpeg(inputFilePath)
-    .outputOptions('-vf', 'scale=-1:360') // 360p
-    .on('end', function() {
-        console.log('processing finished successfully');
-        res.status(200).send('processing finished successfully');
-    })
-    .on('error', function(err: any) {
-        console.log('an error occurred: ' + err.message);
-        res.status(500).send('an error occurred: ' + err.message);
-    })
-    .save(outputFilePath);
+  const inputFileName = data.name;
+  const outputFileName = `processed-${inputFileName}`;
+
+  // download raw video from cloud storage
+  await downloadRawVideo(inputFileName);
+
+  // convert video into 360p
+  try {
+    await convertVideo(inputFileName, outputFileName);
+  } catch (err) {
+    await Promise.all([
+      deleteRawVideo(inputFileName),
+      deleteProcessedVideo(inputFileName)
+    ]);
+    res.status(500).send('processing failed');
+    return
+  }
+
+  // upload video to the cloud
+  await uploadProcessedVideo(outputFileName);
+
+  // clean up local
+  await Promise.all([
+    deleteRawVideo(inputFileName),
+    deleteProcessedVideo(outputFileName)
+  ]);
+
+  res.status(200).send('processing finished successfully');
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`server is running on port ${port}`);
-});
+  console.log(`server is running on port ${port}`);
+})
